d3969becda09e359cad106179d10ab73d8774bf2
LuccasMoragas <luccas_moragas@hotmail.com>
2023-06-30 11:00:31 -0300

    Lista de Contatos funcionando

--- src/pages/Contato.jsx
+++ src/pages/Contato.jsx
@@ -1,28 +1,42 @@
 import { useEffect, useState } from "react";
 import Header from "../components/Header";
 import { db } from "../config/firebase";
-import { getDocs } from "firebase/firestore";
+import { getDocs, collection } from "firebase/firestore";
 
 export default function Contato() {
   const [listaContatos, setListaContatos] = useState([]);
-  const colecaoContatosRef = colecao(db, "contatos");
+  const colecaoContatosRef = collection(db, "contatos");
 
   useEffect(() => {
     const getListaContatos = async () => {
       try {
         const data = await getDocs(colecaoContatosRef);
-        console.log(data);
+        const filteredData = data.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+        }));
+        setListaContatos(filteredData);
       } catch (err) {
         console.error(err);
       }
     };
 
-    getListaContatos;
+    getListaContatos();
   }, []);
 
   return (
-    <div>
+    <div className="Contato">
       <Header />
+
+      <div>
+        {listaContatos.map((contato) => (
+          <div>
+            <h1> Nome: {contato.nome}</h1>
+            <span> Email: {contato.email}</span>
+            <span> Telefone: {contato.telefone}</span>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
