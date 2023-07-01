813c7454a94313ef7c7f9c8f121ad700f6b9303a
LuccasMoragas <luccas_moragas@hotmail.com>
2023-07-01 02:16:40 -0300

    pagina contato css

--- src/pages/Contato.jsx
+++ src/pages/Contato.jsx
@@ -71,53 +71,68 @@ export default function Contato() {
   return (
     <div className="Contato">
       <Header />
-      <div>
-        <input
-          placeholder="Nome"
-          onChange={(e) => setNewContatoNome(e.target.value)}
-        />
-        <input
-          placeholder="Email"
-          onChange={(e) => setNewContatoEmail(e.target.value)}
-        />
-        <input
-          placeholder="Telefone"
-          type="number"
-          onChange={(e) => setNewContatoTelefone(Number(e.target.value))}
-        />
-        <button
-          className="focus:shadow-outline rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700 focus:outline-none"
-          onClick={salvarContato}
+      <div className="flex flex-col items-center justify-center">
+        <form
+          onSubmit={prevent}
+          className="mb-4 w-full max-w-xs rounded-2xl bg-white px-8 pb-8 pt-6 shadow-md"
         >
-          Salvar Contato
-        </button>
-      </div>
-
-      <div>
-        {listaContatos.map((contato) => (
-          <div key={contato.id}>
-            <p> Nome: {contato.nome}</p>
-            <p> Email: {contato.email}</p>
-            <p> Telefone: {contato.telefone}</p>
-            <button
-              className="focus:shadow-outline rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700 focus:outline-none"
-              onClick={() => deletarContato(contato.id)}
-            >
-              Deletar Contato
-            </button>
-
+          <div className="mb-4">
             <input
-              placeholder="Novo Nome"
-              onChange={(e) => setNomeAtualizado(e.target.value)}
+              className="focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none"
+              placeholder="Nome"
+              onChange={(e) => setNewContatoNome(e.target.value)}
             />
+          </div>
+          <div className="mb-6">
+            <input
+              className="focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none"
+              placeholder="Email"
+              onChange={(e) => setNewContatoEmail(e.target.value)}
+            />
+          </div>
+          <div className="mb-8">
+            <input
+              className="focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none"
+              placeholder="Telefone"
+              type="number"
+              onChange={(e) => setNewContatoTelefone(Number(e.target.value))}
+            />
+          </div>
+          <div className="flex items-center justify-evenly">
             <button
               className="focus:shadow-outline rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700 focus:outline-none"
-              onClick={() => atualizarContatoNome(contato.id)}
+              onClick={salvarContato}
             >
-              Atualizar
+              Salvar Contato
             </button>
           </div>
-        ))}
+        </form>
+        <div>
+          {listaContatos.map((contato) => (
+            <div key={contato.id}>
+              <p> Nome: {contato.nome}</p>
+              <p> Email: {contato.email}</p>
+              <p> Telefone: {contato.telefone}</p>
+              <button
+                className="focus:shadow-outline rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700 focus:outline-none"
+                onClick={() => deletarContato(contato.id)}
+              >
+                Deletar Contato
+              </button>
+
+              <input
+                placeholder="Novo Nome"
+                onChange={(e) => setNomeAtualizado(e.target.value)}
+              />
+              <button
+                className="focus:shadow-outline rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700 focus:outline-none"
+                onClick={() => atualizarContatoNome(contato.id)}
+              >
+                Atualizar
+              </button>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
